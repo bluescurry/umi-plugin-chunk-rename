562c77936935fde9961799f81be4dbf56998bf4c
bluescurry <18832643723@163.com>
2021-01-15 20:14:07 +0800

    remove logger

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,9 @@ export default function buildRenamePlugin(api: IApi) {
     // 用户设置了 chunks
     if (chunks && chunks.length > 0) {
       const lastChunk = chunks[chunks.length - 1];
-      api.logger.info(lastChunk);
 
       // 非 umi chunk 则进行处理
       if (lastChunk !== 'umi') {
-        api.logger.info('use plugin', lastChunk);
         config.entry(lastChunk).add(path.join(api.paths.absTmpPath!, 'umi.ts'));
         // 删除原有 umi chunk
         config.entryPoints.delete('umi');
